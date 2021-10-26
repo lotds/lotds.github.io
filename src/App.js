c739a6ba186968240091cd240f570ff5f5b0a45e
Luís Otávio De Simone <luisotaviotorresdesimone@gmail.com>
2021-10-26 00:02:50 -0300

    start react static page dev

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="Home">
+      <header className="Home__header">
+        <h1>Hello World</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
