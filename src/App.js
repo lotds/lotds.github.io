26740fe4d6af486952aab3f9e2b6b2b1920af2d7
Luís Otávio De Simone <85963351+luisotaviodesimone@users.noreply.github.com>
2021-10-27 10:38:29 -0300

    add gap to list items

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
     <div className="mainContent">
       <div className="menuContent">
         <img src="junto-logo-usable.png" alt="logo" className="logo" />
-        <ul className="menuContent">
+        <ul className="menuItems">
+          <h3 className="menuTitle">Cardápio</h3>
           <li>item do menu</li>
           <li>item do menu</li>
           <li>item do menu</li>
